2c9f8cb505f6c20ae4e6df56b6b1b854c8c5b114
larisanchs <larissasanches952@gmail.com>
2026-08-17 20:27:46 -0300

    Redefinição de senha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,20 @@ export const routes: Routes = [
     data: { mode: 'register' },
   },
   {
+    path: 'forgot-password',
+    loadComponent: () =>
+      import('./features/user/forgot-password-page/forgot-password-page').then(
+        (m) => m.ForgotPasswordPage,
+      ),
+  },  
+  {
+  path: 'reset-password',
+  loadComponent: () =>
+    import('./features/user/reset-password-page/reset-password-page').then(
+      (m) => m.ResetPasswordPage,
+    ),
+  },
+  {
     path: 'verify-email',
     loadComponent: () =>
       import('./features/user/verify-email-page/verify-email-page').then((m) => m.VerifyEmailPage),
