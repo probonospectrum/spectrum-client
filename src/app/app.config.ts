4eeda8968bf885c6295a7aabc1542383b82a9935
Hemilly-Pereira <hemillypereirasouza@gmail.com>
2026-09-03 18:25:43 -0300

    importando oidc no app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     providePrimeNG({ theme: { preset: Aura } }),
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideOAuthClient()
   ]
 };
